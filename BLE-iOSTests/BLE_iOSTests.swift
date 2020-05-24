df2a5e8a50044f6119a953a37efd795752aa04d4
jamesjmtaylor <james.jm.taylor@gmail.com>
2020-05-24 08:34:02 -0700

    Initial Commit

---
+++ BLE-iOSTests/BLE_iOSTests.swift
@@ -0,0 +1,34 @@
+//
+//  BLE_iOSTests.swift
+//  BLE-iOSTests
+//
+//  Created by James Taylor on 5/24/20.
+//  Copyright © 2020 James Taylor. All rights reserved.
+//
+
+import XCTest
+@testable import BLE_iOS
+
+class BLE_iOSTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
